d25c24c890c4001138e10c24d4d002a35f742d5c
jongsung_note1 <cjs7478@gmail.com>
2019-01-22 21:43:56 +0900

    최초생성

---
+++ This_is_C/study3.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	char name[20] = "최종성";
+	int old = 46;
+	int length = 175;
+	char blood = 'A';
+
+	printf("10년 후의 프로필...\n");
+	printf("이름: %s\n", name);
+	printf("나이: %d\n", old);
+	printf("키  : %.1lf\n", length - 0.5);
+	printf("혈액형: %c형", blood);
+
+	return 0;
+
+}
